faf48822349d7534b0c3ad845b4d2991c5c74fe7
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-12-08 18:31:47 +0000

    Initialise project

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,59 @@
+import React, { useState, useCallback } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const filterIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch("https://react-hooks-update-eaa56.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = id => {
+    fetch(
+      `https://react-hooks-update-eaa56.firebaseio.com/ingredients/${id}.json`,
+      {
+        method: "DELETE"
+      }
+    ).then(response => {
+      setUserIngredients(prevIngredients =>
+        prevIngredients.filter(ingredient => ingredient.id !== id)
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search onLoadIngredients={filterIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
